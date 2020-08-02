67868defe8deaecce01cc06debef735573c8cbe4
manish978973 <manishrchandran@gmail.com>
2020-08-02 19:41:20 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+   mainurl:"https://docker-backend.azurewebsites.net/api/"
   //mainurl:"http://meannode-env.eba-ttfx9jm3.us-east-2.elasticbeanstalk.com/api/"
-  mainurl:"http://localhost:3000/api/"
+  //mainurl:"http://localhost:3000/api/"
 };
